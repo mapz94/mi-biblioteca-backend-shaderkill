ec41c3ca9a0755b9d52ef1cb717185ca89b0dba5
Miguel Perez <miguel.apg94@gmail.com>
2019-12-06 20:56:46 -0400

    repository

--- src/main/java/com/biblioteca/springboot/backend/models/dao/ISocioDao.java
+++ src/main/java/com/biblioteca/springboot/backend/models/dao/ISocioDao.java
@@ -1,13 +1,16 @@
 package com.biblioteca.springboot.backend.models.dao;
 
 import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 // Imports
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.biblioteca.springboot.backend.models.entity.Socio;
 
 /* Interfaz de solicitudes/respuestas, por ahora usamos las 
  * por defecto incluidas en CrudRepository*/
-public interface ISocioDao extends CrudRepository<Socio, Long> {
+@Repository
+public interface ISocioDao extends JpaRepository<Socio, Long> {
 	
 	@Query("SELECT d FROM Socio d WHERE email =?1 AND password = ?2")
 	public Socio validateSocio(String email, String password);
